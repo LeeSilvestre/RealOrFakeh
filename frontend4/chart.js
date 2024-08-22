00f5422f6aef12afd34350313ecad746dbd47719
LeeSilvestre <150642983+LeeSilvestre@users.noreply.github.com>
2024-08-22 21:23:46 +0800

    what

--- frontend4/chart.js
+++
@@ -1,5 +0,0 @@
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
-
-// Register components
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
-
